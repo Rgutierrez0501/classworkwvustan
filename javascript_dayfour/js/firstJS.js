0e0f829d5565eb64d73b32cd2abc7ff2bf91567e
prabhat1510 <prabhatc@gmail.com>
2022-03-11 04:16:05 +0530

    updated javascript example

---
+++ javascript_dayfour/js/firstJS.js
@@ -0,0 +1,8 @@
+console.log("Hello Everyone"); // It will display the Hello Everyone on the console in browsers developers tool
+document.getElementById('demo1').innerHTML="My First Java Script external file";
+alert("Hey");
+
+//display() is user defined Javascript function
+function display(){
+    document.getElementById('demo2').innerHTML="On click of button i am getting displayed";
+}
---
+++ javascript_dayfour/js/jsFile.js
@@ -0,0 +1,13 @@
+let  d= 15; //d -- is variable whose value 5
+let  e=5; //e -- is a variable whose value 5 or 5.5
+let f=0;
+
+/**Variables can hold many type of data. In JS programming we call it data types.
+1. numbers
+2. strings -- sequence of characters
+3. objects
+**/
+f=d+e; //+ its an addition operator = is an assignment operator
+document.getElementById('id1').innerHTML="add ---"+f;
+f+=d;// += .-= , *=, /=, %=, <<= ,>>=
+document.getElementById('id2').innerHTML="addition assignment operator ---"+f;
