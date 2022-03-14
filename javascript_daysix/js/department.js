fe2e887357dceda070b46e59956162f7b056acbd
prabhat1510 <prabhatc@gmail.com>
2022-03-14 21:01:41 +0530

    code with error to create a module

---
+++ javascript_daysix/js/department.js
@@ -0,0 +1,18 @@
+class Department{
+    /** 
+    constructor(deptId,deptName,deptLocation){
+        this.deptId= deptId;
+        this.deptName= deptName;
+        this.deptLocation=deptLocation;
+    }*/
+
+    setDeptId(deptId){
+        this.deptId = deptId;
+    }
+    setDeptName(deptName){
+        this.deptName = deptName;
+    }
+
+    
+}
+export default function Department();
---
+++ javascript_daysix/js/mainapp.js
@@ -0,0 +1,7 @@
+import {Department} from "./department.js";
+
+let dept = new Department();
+dept.setDeptId(15);
+dept.setDeptName("Marketing");
+
+console.log(dept.deptId+"---dept name---"+dept.deptName);
