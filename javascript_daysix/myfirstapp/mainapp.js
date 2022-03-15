90714064f26b445e3acf73d4db0053e420b3d40b
prabhat1510 <prabhatc@gmail.com>
2022-03-15 15:39:31 +0530

    updated examples

--- javascript_daysix/myfirstapp/mainapp.js
+++ javascript_daysix/myfirstapp/mainapp.js
@@ -6,3 +6,25 @@ d.setDeptId(11);
 d.setDeptName("Marketing");
 
 console.log(d.deptId+"---dept name---"+d.deptName);
+
+// array
+const students = ['John', 'Sara', 'Jack'];
+for (let student of students){
+    console.log(student);
+}
+
+const strVar = "code";
+for(let element of strVar){
+    console.log(element);
+}
+
+const student = {
+    name: 'Monica',
+    class: 7,
+    age: 12
+}
+
+for(key in student){
+    console.log(`${key} => ${student[key]}`);
+}
+console.log(student.name);
