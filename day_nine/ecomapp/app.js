18693dec5c68f18e6877a7ddf07d3abea9027982
prabhat1510 <prabhatc@gmail.com>
2022-03-18 02:01:00 +0530

    updated html code to display data

--- day_nine/ecomapp/app.js
+++ day_nine/ecomapp/app.js
@@ -4,6 +4,7 @@ const app = express();//instance of an express
 const port = 3001;// 
 const queries = require('./queries');
 const path = require('path');
+const { type } = require('os');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,14 +23,7 @@ app.get('/',(req,res)=>{
 //http://localhost:3001/users
 app.get('/users',queries.getUsers);
 
-app.get('/user',(req,res)=>{
-    console.log('Get users from DB----',queries.getUsers);
-    //let user_list = queries.getUsers;
-    let getUsersResult = queries.getUsers;
-    res.render('users',{user_list:getUsersResult().rows});
-   // let today = new Date();
-    //res.render('users',{today:today , user_list:user_list.rows});
-  });
+
 //http://localhost:3001/users/2
 app.get('/users/:id',queries.getUserById);
 
---
+++ day_nine/ecomappsolution/app.js
@@ -0,0 +1,39 @@
+const express = require('express');// including express module in our js file
+const bodyParser = require('body-parser');// including body-parser module in our js file
+const app = express();//instance of an express
+const port = 3001;// 
+const poolconn = require('./databaseconn');
+const path = require('path');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+app.get('/users',(req,res)=>{
+    poolconn.query('SELECT * FROM users ORDER BY id ASC',(error,results)=>{
+        if(error){
+            throw error;
+        }
+        //res.status(200).json(results.rows);
+        res.render('users',{userlist:results.rows});
+    })
+});
+
+app.get('/home',(req,res)=>{
+    res.render('home',{title: 'Home Page'});
+  });
+
+ app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname+'/views/index.html')); // From express js we are sending index.html file to the browser
+  });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+
+
