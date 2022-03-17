a4dce525634ad1e9fc17c71a670a6bb246d1bc1a
prabhat1510 <prabhatc@gmail.com>
2022-03-17 21:54:24 +0530

    updated get users functionality to retrieve data from postgresql

--- day_nine/ecomapp/app.js
+++ day_nine/ecomapp/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3001;
+const queries = require('./queries');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
@@ -13,6 +14,10 @@ app.get('/',(req,res)=>{
     res.json({info: 'Example of Node.js Express and PostGres API'});
 });
 
+//http://localhost:3301/users
+app.get('/users',queries.getUsers);
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
+
--- day_nine/ecomapp/queries.js
+++ day_nine/ecomapp/queries.js
@@ -1,3 +1,4 @@
+//Here we are setting up the configuration of your PostgreSQL connection
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user:'postgres',
@@ -6,3 +7,16 @@ const pool = new Pool({
     password:'password',
     port:5432
 });
+
+const getUsers =(req,res)=>{
+    pool.query('SELECT * FROM users ORDER BY id ASC',(error,results)=>{
+        if(error){
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    })
+}
+
+module.exports={
+    getUsers
+}
