1fbc3a03a0f81109c3c2504307426313313fb138
Rosa Gutierrez <rgutierrez66@go.pasadena.edu>
2022-03-18 08:53:07 -0700

    Enter

---
+++ myapp/arith.js
@@ -0,0 +1,7 @@
+const {add,mul,sub,div}= require ('./arith');
+//First paramet '2+3=5' is the name of the test
+//Second parameter is the function to be run.
+test('2+3=5',()=>{
+    //We are testing that the add function returns correct answer for sample data 
+    expect(add(2,3)).toBe(5);
+})
