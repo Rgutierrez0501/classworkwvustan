f5fcd8a6c582b1769acf09a7581e1d06dbbdbeb7
prabhat1510 <prabhatc@gmail.com>
2022-03-18 22:26:01 +0530

    updated parameterized test cases

--- jestandtypescript_dayten/jestdemo/arith-param.test.js
+++ jestandtypescript_dayten/jestdemo/arith-param.test.js
@@ -12,3 +12,25 @@
         expect(add(a,b)).toBe(expected);
     } 
  );
+
+
+ test.each([[1,1,0],[-1,2,-3],[2,2,0]])(
+    //%i are format specifiers that expect integers
+   '%i  - %i equals %i',(a,b,expected)=>{
+       expect(sub(a,b)).toBe(expected);
+   } 
+);
+
+test.each([[1,1,1],[-1,2,-2],[2,2,4]])(
+    //%i are format specifiers that expect integers
+   '%i  * %i equals %i',(a,b,expected)=>{
+       expect(mul(a,b)).toBe(expected);
+   } 
+);
+
+test.each([[1,1,1],[-1,2,-0.5],[2,2,1]])(
+    //%i are format specifiers that expect integers
+   '%i  / %i equals %i',(a,b,expected)=>{
+       expect(div(a,b)).toBe(expected);
+   } 
+);
