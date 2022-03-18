0eaa10f055e86ba3892b67e714db75b168d51946
prabhat1510 <prabhatc@gmail.com>
2022-03-18 21:31:57 +0530

    updated other test scenarios

--- jestandtypescript_dayten/jestdemo/arith.test.js
+++ jestandtypescript_dayten/jestdemo/arith.test.js
@@ -9,3 +9,12 @@ test('2+3=5',()=>{
 test('3-2=1',()=>{
      expect(sub(3,2)).toBe(1);
 })
+
+test('3*2=6',()=>{
+    expect(mul(3,2)).toBe(6);
+})
+
+
+test('3/2=1.5',()=>{
+    expect(div(3,2)).toBe(1.5);
+})
