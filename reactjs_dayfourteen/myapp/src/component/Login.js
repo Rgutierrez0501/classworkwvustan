81986d957e930d42214277e28338a9afd7ff7e61
prabhat1510 <prabhatc@gmail.com>
2022-03-24 13:56:27 -0400

    updated comments

--- reactjs_dayfourteen/myapp/src/component/Login.js
+++ reactjs_dayfourteen/myapp/src/component/Login.js
@@ -2,6 +2,13 @@ import React, { useState } from "react";
 import "../../src/styles.css";
 
 function Login() {
+    /**
+     * 1. Declare React States for error messages and isSubmitted
+        	We will declare two React states as follows:
+
+		        errorMessages: Store an object with an error message and the name of the field.
+		        isSubmitted: boolean value to indicate if the form is successfully submitted or not.
+     */
   // React States
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -23,6 +30,13 @@ function Login() {
     pass: "invalid password"
   };
 
+  /**
+   * 
+   * Add function to handle form submit
+        To achieve login functionality, we need to create a JS function to handle form submission with validations. 
+        The handleSubmit() function accesses the event object of the form element, event.preventDefault() code 
+        avoids default form submit action which includes reloading of the page.} event 
+   */
   const handleSubmit = (event) => {
     //Prevent page reload
     event.preventDefault();
@@ -47,12 +61,16 @@ function Login() {
   };
 
   // Generate JSX code for error message
+  //The renderErrorMessage function returns JSX code for displaying the error message associated with the field name.
   const renderErrorMessage = (name) =>
     name === errorMessages.name && (
       <div className="error">{errorMessages.message}</div>
     );
 
   // JSX code for login form
+  // We will add JSX code for the HTML form with input type=”text” for both user name and password along with 
+  // input type=”submit” to allow users to submit the form.
+  // Additionally, we will also add error messages below every form input element.
   const renderForm = (
     <div className="form">
       <form onSubmit={handleSubmit}>
