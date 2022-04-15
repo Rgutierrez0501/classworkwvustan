3be24fcd0b20a555a1e32defd08abb12645e5f5f
prabhat1510 <prabhatc@gmail.com>
2022-04-15 16:21:29 -0400

    Updated calculator main app

--- core java/classesdemo/src/classesdemo/ui/CalculatorMain.java
+++ core java/classesdemo/src/classesdemo/ui/CalculatorMain.java
@@ -5,43 +5,50 @@ import java.util.Scanner;
 public class CalculatorMain {
 
 	public static void main(String[] args) {
-		System.out.println("Choose number to perform operations:");
-		System.out.println("1. Addition 2. Subtraction 3.Multiplication 4.Division 5. Modulus Division 6. Exit");
-		Scanner scanner = new Scanner(System.in);
-		Integer choice = scanner.nextInt();
-		
-		System.out.println("Enter two numbers:---");
-		Integer num1 = scanner.nextInt();
-		Integer num2 = scanner.nextInt();
-	
-		Calculator calculator = new Calculator(num1, num2);
-		switch (choice) {
-		case 1:
-			Integer addResult = calculator.addition(num1, num2);
-			System.out.println("Addition of two numbers " + num1 + " and " + num2 + " is " + addResult);
-			break;
-		case 2:
-			Integer subResult = calculator.subtraction(num1, num2);
-			System.out.println("Subtraction of two numbers " + num1 + " and " + num2 + " is " + subResult);
+		Integer num1 = null;
+		Integer num2 = null;
+		Integer choice = 6;
+		boolean running = true;
+		while (running) {
+			System.out.println("Choose number to perform operations:\n");
+			System.out.println("1. Addition \n2. Subtraction \n3. Multiplication \n4. Division \n5. Modulus Division \n6. Exit");
+			Scanner scanner = new Scanner(System.in);
+			choice = scanner.nextInt();
+			if (choice != 6) {
+				System.out.println("Enter two numbers:---\n");
+				num1 = scanner.nextInt();
+				num2 = scanner.nextInt();
+			}
+			Calculator calculator = new Calculator(num1, num2);
+			switch (choice) {
+			case 1:
+				Integer addResult = calculator.addition(num1, num2);
+				System.out.println("Addition of two numbers " + num1 + " and " + num2 + " is " + addResult);
+				break;
+			case 2:
+				Integer subResult = calculator.subtraction(num1, num2);
+				System.out.println("Subtraction of two numbers " + num1 + " and " + num2 + " is " + subResult);
 
-			break;
-		case 3:
-			Integer mulResult = calculator.multiplication(num1, num2);
-			System.out.println("Multiplication of two numbers " + num1 + " and " + num2 + " is " + mulResult);
-			break;
-		case 4:
-			Integer divResult = calculator.division(num1, num2);
-			System.out.println("Division of two numbers " + num1 + " and " + num2 + " is " + divResult);
-			break;
-		case 5:	Integer modDivResult = calculator.modulusDivision(num1, num2);
+				break;
+			case 3:
+				Integer mulResult = calculator.multiplication(num1, num2);
+				System.out.println("Multiplication of two numbers " + num1 + " and " + num2 + " is " + mulResult);
+				break;
+			case 4:
+				Integer divResult = calculator.division(num1, num2);
+				System.out.println("Division of two numbers " + num1 + " and " + num2 + " is " + divResult);
+				break;
+			case 5:
+				Integer modDivResult = calculator.modulusDivision(num1, num2);
 				System.out.println("Modulus Division of two numbers " + num1 + " and " + num2 + " is " + modDivResult);
 				break;
-		default:
+			default:
+				System.out.println("You no longer will be able to use calculator");
+				running=false;
 				break;
 
+			}
+
 		}
-
-		
 	}
-
 }
